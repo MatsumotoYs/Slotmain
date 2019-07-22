410ef488f80af774a0912795a94d75c26be4d72d
Matsumoto <ocs.matsumoto.yoshiki@gmail.com>
2019-07-22 23:02:09 +0900

    kousin

--- src/slot/Slotmain1.java
+++ src/slot/Slotmain1.java
@@ -1,45 +1,52 @@
 package slot;
 
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.EventQueue;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Random;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JButton;
 import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
 
 public class Slotmain1 extends JFrame implements ActionListener, Runnable {
+
+
+
 
 	JButton btnStart;
 	JButton btnStop;
 	JButton btnStop_1;
 	JButton btnStop_2;
 
-	boolean stop = false;
+//	boolean stop = false;
+	boolean stop1;
+	boolean stop2;
+	boolean stop3;
 
 	private JPanel contentPane;
+
+//	private Container ct;
 
 	/**
 	 * Launch the application.
 	 */
 
-	private String[] list = { ("1"), ("2"), ("3"), ("4"), ("5"), ("6"), ("7"), ("8"), ("9")
+
+	private String[] list = { ("0"), ("1"), ("2"), ("3"), ("4"), ("5"), ("6"), ("7"), ("8"), ("9")
 
 	};
 
 	private JTextField textField;
 	private JTextField textField_1;
 	private JTextField textField_2;
-	
+
+
 
 	public static void main(String[] args) {
-		Slotmain1 i = new Slotmain1("スロットん");
+		Slotmain1 i = new Slotmain1("JAVAスロット");
 		i.setVisible(true);
 //		EventQueue.invokeLater(new Runnable() {
 //			public void run() {
@@ -59,7 +66,7 @@ public class Slotmain1 extends JFrame implements ActionListener, Runnable {
 	public Slotmain1(String title) {
 		super(title);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 452, 350);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -71,113 +78,201 @@ public class Slotmain1 extends JFrame implements ActionListener, Runnable {
 //			public void actionPerformed(ActionEvent e) {
 //				if (e.getSource() == btnStart) {
 //					stop = false;
-//					
+//
 //				}
 //			}
 //		});
-		btnStart.setBounds(12, 223, 101, 25);
+		btnStart.setBounds(160, 256, 110, 25);
 		contentPane.add(btnStart);
 
-		btnStop = new JButton("Stop");
+		btnStop = new JButton("Stop");										// stopボタン１
 		btnStop.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				if (e.getSource() == btnStop) {
-					stop = true;
+					stop1 = true;
 				}
 				;
 			}
 		});
-		btnStop.setBounds(39, 183, 101, 25);
+		btnStop.setBounds(25, 220, 110, 25);
 		contentPane.add(btnStop);
-		
-//		btnStop_1 = new JButton("Stop2");
-//		btnStop_1.addActionListener(new ActionListener() {
-//			public void actionPerformed(ActionEvent e) {
-//				if (e.getSource() == btnStop) {
-//					stop = true;
-//				}
-//			}
-//			
-//		});
-//		btnStop_1.setBounds(159, 183, 101, 25);
-//		contentPane.add(btnStop_1);
+
+		btnStop_1 = new JButton("Stop2");									// stopボタン2
+		btnStop_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (e.getSource() == btnStop_1) {
+					stop2 = true;
+				}
+			}
+
+		});
+		btnStop_1.setBounds(160, 220, 110, 25);
+		contentPane.add(btnStop_1);
+
+
+
+		btnStop_2 = new JButton("Stop3");									// stopボタン３
+		btnStop_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (e.getSource() == btnStop_2) {
+					stop3 = true;
+				}
+			}
+		});
+		btnStop_2.setBounds(295, 220, 110, 25);
+		contentPane.add(btnStop_2);
+
 
 		textField = new JTextField();
-		textField.setBounds(29, 61, 118, 109);
+		textField.setHorizontalAlignment(JTextField.CENTER);
+		textField.setBounds(25, 80, 110, 110);
 		contentPane.add(textField);
 		textField.setColumns(10);
 
 		textField_1 = new JTextField();
-		textField_1.setBounds(159, 61, 101, 108);
+		textField_1.setHorizontalAlignment(JTextField.CENTER);
+		textField_1.setBounds(160, 80, 110, 110);
 		contentPane.add(textField_1);
 		textField_1.setColumns(10);
 
 		textField_2 = new JTextField();
-		textField_2.setBounds(272, 61, 111, 109);
+		textField_2.setHorizontalAlignment(JTextField.CENTER);
+		textField_2.setBounds(295, 80, 110, 110);
 		contentPane.add(textField_2);
 		textField_2.setColumns(10);
-		
 
-//		
-//		btnStop_2 = new JButton("Stop3");
-//		btnStop_2.addActionListener(new ActionListener() {
-//			public void actionPerformed(ActionEvent e) {
-//			}
-//		});
-//		btnStop_2.setBounds(282, 183, 101, 25);
-//		contentPane.add(btnStop_2);
+
+
 
 	}
 
 	@Override
 	public void run() {
+//		ct = getContentPane();
+		textField.setFont(new Font("MS ゴシック",Font.BOLD,100));
+		textField_1.setFont(new Font("MS ゴシック",Font.BOLD,100));
+		textField_2.setFont(new Font("MS ゴシック",Font.BOLD,100));
+
+
+
+//		ct.add(textField, SwingConstants.CENTER);
+//		ct.add(textField_1, SwingConstants.CENTER);
+//		ct.add(textField_2, SwingConstants.CENTER);
+
 		Random random = new Random();
 		int x = random.nextInt(list.length);
 		int y = random.nextInt(list.length);
 		int z = random.nextInt(list.length);
 
-		// スロット処理
-		while (true) {
-			// テキストを描写
-			textField.setText(list[x]);
-			textField_1.setText(list[y]);
-			textField_2.setText(list[z]);
+		btnStop.setEnabled(true);
+		btnStop_1.setEnabled(true);
+		btnStop_2.setEnabled(true);
 
-			// 終了フラグ判定(stopボタンが押されたら...)
-			if (stop) {
+		stop1 = false;
+		stop2 = false;
+		stop3 = false;
+
+		while(true)	{
+			x++;
+			if(x == list.length)
+				x = 1;
+
+			y++;
+			if(y == list.length)
+				y = 1;
+
+			z++;
+			if(z == list.length)
+				z = 1;
+
+			if(!(stop1))textField.setText(Integer.toString(x));
+			if(!(stop2))textField_1.setText(Integer.toString(y));
+			if(!(stop3))textField_2.setText(Integer.toString(z));
+
+			if(stop1 && stop2 && stop3)	{
 				break;
 			}
+			try	{
+				Thread.sleep(200);
+			} catch (InterruptedException e1)	{
 
-			try {
-				Thread.sleep(30);
-			} catch (InterruptedException e1) {
-				// TODO 自動生成された catch ブロック
 			}
 
-			x++;
-			// リストの最後までいくと、0に戻す
-			if (x == list.length) {
-				x = 0;
-			}
-			x = random.nextInt(list.length);
-			y = random.nextInt(list.length);
-			z = random.nextInt(list.length);
 		}
+
+
+//		// スロット処理
+//		while (true) {
+//			// テキストを描写
+//			textField.setText(list[x]);
+//			textField_1.setText(list[y]);
+//			textField_2.setText(list[z]);
+//
+//			// 終了フラグ判定(stopボタンが押されたら...)
+//			if (stop) {
+//				break;
+//
+//			}
+//
+//			try {
+//				Thread.sleep(300);
+//			} catch (InterruptedException e1) {
+//				// TODO 自動生成された catch ブロック
+//			}
+//
+//			x++;
+//			// リストの最後までいくと、0に戻す
+//			if (x == list.length) {
+//				x = 0;
+//			}
+//
+//			y++;
+//			if (y == list.length)	{
+//				y = 0;
+//			}
+//
+//			z++;
+//			if (z == list.length)	{
+//				z = 0;
+//			}
+//
+////			x = random.nextInt(list.length);
+////			y = random.nextInt(list.length);
+////			z = random.nextInt(list.length);
+////			x++;
+//
+//		}
+//	}
+//
+//
+//	@Override
+//	public void actionPerformed(ActionEvent e) {
+//		// TODO 自動生成されたメソッド・スタブ
+//		if (e.getSource() == btnStart) {
+//			stop = false;
+//			Thread th = new Thread(this);
+//			th.start();
+//		} else if (e.getSource() == btnStop) {
+//			stop = true;
+//
+//		}
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO 自動生成されたメソッド・スタブ
 		if (e.getSource() == btnStart) {
-			stop = false;
+			stop1 = false;
 			Thread th = new Thread(this);
 			th.start();
 		} else if (e.getSource() == btnStop) {
-			stop = true;
-			
+			stop1 = true;
+
 		}
+
 	}
-	
-	
+
+
+
 
 }
